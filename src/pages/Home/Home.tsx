80d865c4e6024534d93846fce44ec059bcafc5b4
Ahmad Faiz Agustianto <107536877+Mifaki@users.noreply.github.com>
2024-02-15 16:14:25 +0700

    `feat(core): Create Home Page`

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+            <main className="h-screen w-full flex flex-col gap-6">
+                <h1 className="display-1 text-accent font-bold">Quizlet</h1>
+                <p className="body-1 text-center">Your all in one quiz place</p>
+            </main>
+    )
+}
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "2rem",
+        sm: "3rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
-        primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a" }
-      }
+        background: "#0C0A09",
+        "text-primary": "#F2F2F2",
+        card: "#1C1917",
+        accent: "#E11D48",
+        "accent-dark": "#E40939",
+        "accent-blue": "#1446A0",
+        "accent-blue-dark": "#1446A0",
+        "accent-yellow": "#1446A0",
+        "accent-yellow-dark": "#1446A0",
+        "accent-turq": "#1446A0",
+        "accent-turq-dark": "#1446A0",
+        "accent-pruple": "#1446A0",
+        "accent-pruple-dark": "#1446A0"
+      },
+      fontFamily: {
+        sans: [
+          'Poppins, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+          { fontFeatureSettings: '"cv02", "cv03", "cv04", "cv11"' },
+        ],
+      },
     },
   },
   plugins: [],
-}
-
+};
